0029c0c8ea99f1698a7541345bbc6e188c4c0b91
Sami Korpela <skipadu@users.noreply.github.com>
2018-06-09 18:00:28 +0300

    Button now shows joke retrieved from the joke-supplier

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.pihrit.joke_supplier.JokeSupplier;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        JokeSupplier jokeSupplier = new JokeSupplier();
+        String joke = jokeSupplier.getJoke();
+        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
     }
 
 
--- joke-supplier/src/main/java/com/pihrit/joke_supplier/JokeSupplier.java
+++ joke-supplier/src/main/java/com/pihrit/joke_supplier/JokeSupplier.java
@@ -1,4 +1,9 @@
 package com.pihrit.joke_supplier;
 
 public class JokeSupplier {
+
+    public String getJoke() {
+        return "Why are astronauts so calm? - No pressure in vacuum.";
+    }
+
 }
