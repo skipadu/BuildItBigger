9a525b9710402c98e6f33c2a0fb0ce1987c2025e
Sami Korpela <skipadu@users.noreply.github.com>
2018-06-10 17:36:24 +0300

    Added few jokes and randomly choosing between them

--- joke-supplier/src/main/java/com/pihrit/joke_supplier/JokeSupplier.java
+++ joke-supplier/src/main/java/com/pihrit/joke_supplier/JokeSupplier.java
@@ -1,9 +1,21 @@
 package com.pihrit.joke_supplier;
 
+import java.util.Random;
+
 public class JokeSupplier {
 
+    public final String allJokes[] = new String[]{
+            "Why are astronauts so calm? \n- No pressure in vacuum.",
+            "What do you call a programmer from Scandinavia? \n- Nerdic.",
+            "A programmer had a problem. He decided to user Java to solve it. \n-He now has a ProblemFactory"
+    };
+
     public String getJoke() {
-        return "Why are astronauts so calm? \n- No pressure in vacuum.";
+        return allJokes[getRandomIndex()];
+    }
+
+    private int getRandomIndex() {
+        return new Random().nextInt(allJokes.length);
     }
 
 }
