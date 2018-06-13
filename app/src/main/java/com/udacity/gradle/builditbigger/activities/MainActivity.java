b9803eada6df0b6116ada144d5406c20c05da67c
Sami Korpela <skipadu@users.noreply.github.com>
2018-06-13 20:05:14 +0300

    Removed the unused settings menu

--- app/src/main/java/com/udacity/gradle/builditbigger/activities/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/activities/MainActivity.java
@@ -9,8 +9,6 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.test.espresso.idling.CountingIdlingResource;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 
@@ -52,29 +50,6 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         Timber.plant(new Timber.DebugTree());
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void tellJoke(View v) {
